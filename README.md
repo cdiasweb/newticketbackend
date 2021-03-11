d4af6346dec96069faad2d9ad8bec9c21b299cd3
Carlos Dias <carlosdias.web@gmail.com>
2021-03-11 00:11:47 -0300

    Finalizando rota para registrar uma empresa

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # newticketbackend
 Backend to new ticket project
+
+Developing envirorment
+1. curl -s https://laravel.build/newticketbackend | bash
---
+++ app/Models/Customer.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Customer extends Model
+{
+
+    protected $guarded = [];
+
+    public function company() 
+    {
+        return $this->belongsTo(Company::class);
+    }
+
+    public function events()
+    {
+        return $this->hasMany(Event::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
